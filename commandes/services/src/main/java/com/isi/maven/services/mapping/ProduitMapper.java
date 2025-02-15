20e7e997a6e518c6a48df90077ffe02be3aeea7c
mainProgram <fazeyfadiop@gmail.com>
2025-02-15 15:49:42 +0000

    save user id while updating produit

--- commandes/services/src/main/java/com/isi/maven/services/mapping/ProduitMapper.java
+++ commandes/services/src/main/java/com/isi/maven/services/mapping/ProduitMapper.java
@@ -1,6 +1,7 @@
 package com.isi.maven.services.mapping;
 
 import com.isi.maven.services.dto.Produit;
+import com.isi.maven.services.entities.AppUserEntity;
 import com.isi.maven.services.entities.ProduitEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,5 +11,12 @@ public interface ProduitMapper {
     @Mapping(target = "appUserId", source = "appUserEntity.id")
     Produit toProduit(ProduitEntity produitEntity);
 
+    @Mapping(target = "appUserEntity", source = "produit.appUserId")
     ProduitEntity fromProduit(Produit produit);
+
+    default AppUserEntity map(int appUserId) {
+        AppUserEntity appUserEntity = new AppUserEntity();
+        appUserEntity.setId(appUserId);
+        return appUserEntity;
+    }
 }
