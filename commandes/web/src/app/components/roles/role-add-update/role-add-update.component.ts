f4ebe8130cb5e2d6138e05a8e6882b5e0aebfe7b
mainProgram <fazeyfadiop@gmail.com>
2025-02-15 17:48:20 +0000

    crud roles complete

--- commandes/web/src/app/components/roles/role-add-update/role-add-update.component.ts
+++ commandes/web/src/app/components/roles/role-add-update/role-add-update.component.ts
@@ -1,12 +1,121 @@
-import { Component } from '@angular/core';
+import {Component, inject, Inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef
+} from "@angular/material/dialog";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
+import {MatSelect} from "@angular/material/select";
+import {MatToolbar} from "@angular/material/toolbar";
+import {NgForOf, NgIf} from "@angular/common";
+import {RolePipePipe} from "../../../shared/pipes/role-pipe.pipe";
+import {AppRoleService} from "../../../services/app-role.service";
+import {take} from "rxjs";
+import {AppRoles} from "../../../models/app-roles.model";
 
 @Component({
   selector: 'app-role-add-update',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule,
+        MatButton,
+        MatDialogActions,
+        MatDialogClose,
+        MatDialogContent,
+        MatError,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        MatOption,
+        MatProgressSpinner,
+        MatSelect,
+        MatToolbar,
+        NgForOf,
+        NgIf,
+        ReactiveFormsModule,
+        RolePipePipe
+    ],
   templateUrl: './role-add-update.component.html',
   styleUrl: './role-add-update.component.css'
 })
-export class RoleAddUpdateComponent {
+export class RoleAddUpdateComponent implements OnInit{
+  form!: FormGroup;
+  errorMessage: string = "";
+  isLoading= false
+
+  constructor(
+    private _roleService: AppRoleService,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<RoleAddUpdateComponent>,
+  ) {
+    this._roleService.fetchAppRoles().pipe(
+      take(1),
+    ).subscribe();
+  }
+
+  ngOnInit(): void {
+    this.initForm()
+    if(this.data?.id)
+      this.populateForm()
+  }
+
+  initForm(){
+    this.form = this.fb.group({
+      nom: ['', Validators.required],
+    });
+  }
+
+  populateForm(){
+    this.form = this.fb.group({
+      nom: [this.data?.nom, Validators.required],
+    });
+  }
+
+  get roles() {
+    return this._roleService.resources;
+  }
+
+  onSubmit(){
+    this.isLoading = true
+    if(this.data?.id) {
+      this._roleService.updateAppRole(this.data?.id, this.form.value).subscribe({
+        next: value => {
+          console.log(value)
+          this.isLoading = false
+          this.errorMessage = "";
+          this.dialogRef.close(true)
+        },
+        error: (error) => {
+          console.log(error)
+          this.isLoading = false
+          this.errorMessage = error?.error?.error;
+        }
+      })
+    }
+    else{
+      let body = this.form.value as AppRoles
+      this._roleService.addAppRole(body).subscribe({
+        next: value => {
+          this.errorMessage = "";
+          this.isLoading = false
+          console.log(value)
+          this.dialogRef.close(true)
+        },
+        error: (error) => {
+          console.log(error)
+          this.isLoading = false
+          this.errorMessage = error?.error?.error;
+        }
+      })
+    }
+  }
 
 }
--- commandes/web/src/app/components/roles/role-list/role-list.component.ts
+++ commandes/web/src/app/components/roles/role-list/role-list.component.ts
@@ -1,12 +1,102 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {LoaderService} from "../../../services/loader.service";
+import {MatDialog} from "@angular/material/dialog";
+import {finalize, take} from "rxjs";
+import Swal from "sweetalert2";
+import {RoleAddUpdateComponent} from "../role-add-update/role-add-update.component";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {NgForOf, NgIf} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+import {LoaderComponent} from "../../loader/loader.component";
+import {AppRoleService} from "../../../services/app-role.service";
+import {AppRoles} from "../../../models/app-roles.model";
+import {RolePipePipe} from "../../../shared/pipes/role-pipe.pipe";
 
 @Component({
   selector: 'app-role-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardHeader,
+    NgForOf,
+    MatIcon,
+    MatCardTitle,
+    MatCardActions,
+    MatCard,
+    MatCardContent,
+    RouterLink,
+    LoaderComponent,
+    NgIf, RolePipePipe
+  ],
   templateUrl: './role-list.component.html',
   styleUrl: './role-list.component.css'
 })
 export class RoleListComponent {
 
+  router = inject(Router);
+  roleService = inject(AppRoleService);
+  loaderService = inject(LoaderService);
+  loading = inject(LoaderService).loading;
+
+  constructor(
+    public dialog: MatDialog,
+  ) {
+    this.loaderService.showLoader()
+    this.roleService.fetchAppRoles().pipe(
+      take(1),
+      finalize(() => this.loaderService.hideLoader())
+    ).subscribe();
+  }
+
+  get roles() {
+    return this.roleService.resources;
+  }
+
+  deleteRole(id: number|undefined){
+    Swal.fire({
+      title: 'Suppression',
+      text: 'Voulez-vous vraiment vous supprimer le rôle ?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Supprimer',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.value) {
+        this.roleService.deleteAppRole(id ?? 0).subscribe({
+          next: (data) => {
+            console.log(data)
+            Swal.fire(
+              'Succès',
+              'Suppression réussie !',
+              'success'
+            )
+          }
+        })
+      }
+    })
+  }
+
+  add() {
+    const dialogRef = this.dialog.open(RoleAddUpdateComponent, {
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        Swal.fire("Enregistrement effectué !")
+      }
+    });
+  }
+
+  edit(data: AppRoles) {
+    const dialogRef = this.dialog.open(RoleAddUpdateComponent, {
+      width: '600px',
+      data: data
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        Swal.fire("Enregistrement effectué !")
+      }
+    });
+  }
 }
