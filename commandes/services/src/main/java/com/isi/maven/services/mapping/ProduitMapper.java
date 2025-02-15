374ef70ccae3b0b1d9ec3b8305ef85dc5d6d82f2
mainProgram <fazeyfadiop@gmail.com>
2025-02-15 15:42:43 +0000

    return user id in backend service when fetch all

--- commandes/services/src/main/java/com/isi/maven/services/mapping/ProduitMapper.java
+++ commandes/services/src/main/java/com/isi/maven/services/mapping/ProduitMapper.java
@@ -3,9 +3,12 @@ package com.isi.maven.services.mapping;
 import com.isi.maven.services.dto.Produit;
 import com.isi.maven.services.entities.ProduitEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProduitMapper {
+    @Mapping(target = "appUserId", source = "appUserEntity.id")
     Produit toProduit(ProduitEntity produitEntity);
+
     ProduitEntity fromProduit(Produit produit);
 }
